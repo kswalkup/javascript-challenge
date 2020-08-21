29693eb96c5c9976bda7351ddd1c2805cd4b50d0
UT_Boot_Camp <bc.kswalkup@gmail.com>
2020-08-20 23:42:07 -0400

    update app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,7 +1,7 @@
 // from data.js
 var tableData = data;
-
-// YOUR CODE HERE!
+var button = d3.select("#filter-btn");
+var form = d3.select("#form-group");
 var tbody = d3.select("tbody");
 
 data.forEach(function(ufoReport){
@@ -13,36 +13,31 @@ data.forEach(function(ufoReport){
     });
 });
 
-var button = d3.select("#filter-btn");
-
-// Select the form
-var form = d3.select("#form-group");
-
-// Create event handlers 
 button.on("click", runEnter);
 form.on("submit",runEnter);
+
 
 function runEnter() {
 
     d3.event.preventDefault();
     // Select the input element and get the raw HTML node
-var inputElement = d3.select('#datetime');
+    tbody.html("")
+    var inputElement = d3.select('#datetime');
     // Get the value property of the input element
-var inputValue = inputElement.property('value')
+    var inputValue = inputElement.property('value')
 
-var datetimeFilteredData = tableData.filter(ufoData=> ufoData.datetime === inputValue);
-console.log(datetimeFilteredData);
+    var dateTimeData = tableData.filter(ufoData=> ufoData.datetime === inputValue);
+    console.log(dateTimeData);
 
-
-
-
-// var list = d3.select('thead')
-
-// list.append('th').text(datetimeFilteredData);
-// 
-
-
-
+    dateTimeData.forEach(function(ufoDateReport){
+        console.log(ufoDateReport);
+        var row = tbody.append('tr');
+        Object.values(ufoDateReport).forEach(function (value){
+            console.log(value);
+            tbody.append('td').text(value);
+        });
+    });
 
 };
+
 
